984dbd67f9e5ac97e36acfee4f177d359a68c0c2
Javier Ruiz <jruiz@spartan-sys.com>
2016-07-28 16:28:43 +0200

    Septimo commit, optimizando fontawsome en la linea de Bootstrap, haciendo todas las optimizaciones en gulpfile, separando imagenes entre src y dist, y añadiendo imagenes adaptadas a cada densidad 1x, 2x, 3x. este commit es previo al uso de srcset

--- Intento_1/gulpfile.js
+++ Intento_1/gulpfile.js
@@ -6,17 +6,26 @@ var browserSync = require('browser-sync').create();
 var browserify = require('browserify');
 var tap = require('gulp-tap');
 var buffer = require('gulp-buffer');
+var uglify = require('gulp-uglify');
+var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssnano = require('cssnano');
+var imagemin = require('gulp-imagemin');
+var spritesmith = require('gulp.spritesmith');
 
 // variables de patrones de archivos
 var jsFiles = ["src/js/*.js", "src/js/**/*.js"];
+var uploadedImages = ["uploads/*.png", "uploads/*.jpg", "uploads/*.gif", "uploads/*.svg"];
+var assetsImages = ["src/img/*.png", "src/img/*.jpg", "src/img/*.gif", "src/img/*.svg"];
 
 // definimos tarea por defecto
-gulp.task("default", ["concat-js", "compile-sass"], function(){
+gulp.task("default", ["concat-js", "compile-sass", "assets-images-optimization"], function(){
 
     // iniciar BrowserSync
     browserSync.init({
-        //server: "./", // levanta servidor web en carpeta actual
-        proxy: "127.0.0.1:8000",
+        // server: "./", // levanta servidor web en carpeta actual
+        proxy: "127.0.0.1:8000",  // actúa como proxy enviando las peticiones a sparrest
         browser: "google chrome"
     });
 
@@ -29,16 +38,25 @@ gulp.task("default", ["concat-js", "compile-sass"], function(){
     // observar cambios en archivos JS para concatenar
     gulp.watch(jsFiles, ["concat-js"]);
 
+    // observar cambios en los assets para optimizarlos
+    gulp.watch(assetsImages, ["assets-images-optimization"]);
+
 });
 
 // definimos la tarea para compilar SASS
 gulp.task("compile-sass", function(){
     gulp.src("./src/scss/style.scss") // cargamos le archivo
+        .pipe(sourcemaps.init()) // comenzamos la captura de sourcemaps
         .pipe(sass().on('error', sass.logError)) // compilamos el archivo SASS
+        .pipe(postcss([
+            autoprefixer(), // autoprefija automáticamente el CSS
+            cssnano() // minifica el CSS
+        ]))
+        .pipe(sourcemaps.write('./')) // escribimos los sourcemaps
         .pipe(gulp.dest("./dist/css/")) // guardamos el archivo en dist/css
         .pipe(notify({
             title: "SASS",
-            message: "Compiled OK"
+            message: "Compiled Ok"
         }))
         .pipe(browserSync.stream());
 });
@@ -46,10 +64,13 @@ gulp.task("compile-sass", function(){
 // definimos la tarea para concatenar JS
 gulp.task("concat-js", function(){
     gulp.src("src/js/app.js")
+        .pipe(sourcemaps.init()) // comenzamos la captura de sourcemaps
         .pipe(tap(function(file){ // tap nos permite ejecutar un código por cada fichero seleccionado en el paso anterior
-            file.contents = browserify(file.path, {debug:true}).bundle(); // pasamos el archivo por browserify para importar los require
+            file.contents = browserify(file.path).bundle(); // pasamos el archivo por browserify para importar los require
         }))
         .pipe(buffer()) // convertir cada archivo en un stream
+        .pipe(uglify()) // minifica el javascript
+        .pipe(sourcemaps.write('./')) // escribimos los sourcemaps
         .pipe(gulp.dest("dist/js/"))
         .pipe(notify({
             title: "JS",
@@ -57,3 +78,18 @@ gulp.task("concat-js", function(){
         }))
         .pipe(browserSync.stream());
 });
+
+// optimización de imagenes de usuario
+gulp.task("uploaded-images-optimization", function(){
+    gulp.src(uploadedImages)
+        .pipe(imagemin())
+        .pipe(gulp.dest('./uploads/'));
+});
+
+// optimizacion de assets + sprites
+gulp.task("assets-images-optimization", function(){
+    gulp.src(assetsImages)
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/img/'));
+});
+
